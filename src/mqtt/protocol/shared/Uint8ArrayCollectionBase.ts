812f799421bb9cacdf705b8f8add3bb6e12dd7cc
Grzegorz Łagocki <grzesieklagocki@gmail.com>
2025-08-27 15:44:18 +0200

    refactor: Add comments to public properties of `Uint8ArrayCollectionBase` class

--- src/mqtt/protocol/shared/Uint8ArrayCollectionBase.ts
+++ src/mqtt/protocol/shared/Uint8ArrayCollectionBase.ts
@@ -1,19 +1,25 @@
 export abstract class Uint8ArrayCollectionBase {
   private _index = 0;
 
+  // Total capacity of the buffer
   public get capacity(): number {
     return this.array.length;
   }
 
+  // Number of bytes that can still be written/read
   public get remaining(): number {
     return this.capacity - this.index;
   }
 
+  /**
+   * Creates a new instance of the class with the provided Uint8Array.
+   * @param array Byte array to be stored in the collection.
+   */
+  constructor(protected readonly array: Uint8Array) {}
+
   protected get index(): number {
     return this._index;
   }
-
-  constructor(protected readonly array: Uint8Array) {}
 
   protected canProcess(bytesCount: number): boolean {
     if (bytesCount < 1) throw Error("Bytes count must be at least 1");
