0ac83d87fcbce395abcdaec12a698a83cafa6488
Grzegorz Łagocki <grzesieklagocki@gmail.com>
2025-08-27 22:17:18 +0200

    feat(v4/packet-types): Add MQTT Control Packet types

---
+++ src/mqtt/protocol/shared/types.ts
@@ -0,0 +1,25 @@
+export enum PacketType {
+  CONNECT = 1, // Client request to connect to Server
+  CONNACK = 2, // Connect acknowledgment
+  PUBLISH = 3, // Publish message
+  PUBACK = 4, // Publish acknowledgment
+  PUBREC = 5, // Publish received (assured delivery part 1)
+  PUBREL = 6, // Publish release (assured delivery part 2)
+  PUBCOMP = 7, // Publish complete (assured delivery part 3)
+  SUBSCRIBE = 8, // Client subscribe request
+  SUBACK = 9, // Subscribe acknowledgment
+  UNSUBSCRIBE = 10, // Unsubscribe request
+  UNSUBACK = 11, // Unsubscribe acknowledgment
+  PINGREQ = 12, // PING request
+  PINGRESP = 13, // PING response
+  DISCONNECT = 14, // Client is disconnecting
+}
+
+// BASE
+export type ControlPacket<T extends PacketType> = {
+  typeId: T;
+};
+
+export type PacketWithIdentifier<T extends PacketType> = ControlPacket<T> & {
+  identifier: number;
+};
---
+++ src/mqtt/protocol/v4/types.ts
@@ -0,0 +1,94 @@
+import {
+  ControlPacket,
+  PacketType,
+  PacketWithIdentifier,
+} from "../shared/types";
+
+// 1. CONNECT
+export type ConnectPacketV4 = ControlPacket<PacketType.CONNECT> & {
+  protocol: ProtocolInfoV4;
+  flags: ConnectionFlagsV4;
+
+  keepAlive: number;
+};
+
+export type ProtocolInfoV4 = {
+  name: "MQTT";
+  level: 4;
+};
+
+export type ConnectionFlagsV4 = {
+  userName: boolean;
+  password: boolean;
+  willRetain: boolean;
+  willQoS: 0 | 1 | 2;
+  willFlag: boolean;
+  cleanSession: boolean;
+};
+
+export type ConnectionPayloadV4 = {
+  clientIdentifier?: string;
+  willTopic?: string;
+  willMessage?: Uint8Array;
+  userName?: string;
+  password?: Uint8Array;
+};
+
+// 2. CONNACK
+export type ConnackPacketV4 = ControlPacket<PacketType.CONNACK> & {
+  sessionPresentFlag: boolean;
+  connectReturnCode: 0x00 | 0x01 | 0x02 | 0x03 | 0x04 | 0x05;
+};
+
+// 3. PUBLISH
+export type PublishPacketV4 = PacketWithIdentifier<PacketType.PUBLISH> & {
+  flags: PublishFlagsV4;
+
+  topicName: string;
+  applicationMessage: Uint8Array;
+};
+
+export type PublishFlagsV4 = {
+  dup: boolean;
+  qosLevel: 0 | 1 | 2;
+  retain: boolean;
+};
+
+// 4. PUBACK
+export type PubackPacketV4 = PacketWithIdentifier<PacketType.PUBACK>;
+
+// 5. PUBREC
+export type PubrecPacketV4 = PacketWithIdentifier<PacketType.PUBREC>;
+
+// 6. PUBREL
+export type PubrelPacketV4 = PacketWithIdentifier<PacketType.PUBREL>;
+
+// 7. PUBCOMP
+export type PubcompPacketV4 = PacketWithIdentifier<PacketType.PUBCOMP>;
+
+// 8. SUBSCRIBE
+export type SubscribePacketV4 = PacketWithIdentifier<PacketType.SUBSCRIBE> & {
+  subscriptionList: [topicFilter: string, qos: 0 | 1 | 2][];
+};
+
+// 9. SUBACK
+export type SubackPacketV4 = PacketWithIdentifier<PacketType.SUBACK> & {
+  returnCode: 0x00 | 0x01 | 0x02 | 0x80;
+};
+
+// 10. UNSUBSCRIBE
+export type UnsubscribeV4 = PacketWithIdentifier<PacketType.UNSUBSCRIBE> & {
+  topicList: string[];
+};
+
+// 11. UNSUBACK
+export type UnsubackPacketV4 = PacketWithIdentifier<PacketType.UNSUBACK>;
+
+// 12. PINGREQ
+export type PingreqPacketV4 = ControlPacket<PacketType.PINGREQ>;
+
+// 13. PINGRESP
+export type PingrespPacketV4 = ControlPacket<PacketType.PINGRESP>;
+
+// 14. DISCONNECT
+export type DisconnectPacketV4 = ControlPacket<PacketType.DISCONNECT>;
