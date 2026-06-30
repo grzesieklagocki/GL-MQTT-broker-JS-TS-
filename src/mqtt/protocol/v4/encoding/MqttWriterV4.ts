a2fc85154cb509c9f1a61688b879cb780ef38f06
Grzegorz Łagocki <grzesieklagocki@gmail.com>
2026-07-01 01:26:02 +0200

    feat(MqttWriterV4): Implement methods for writing integers in `MqttWriterV4` class (with tests)

---
+++ src/mqtt/protocol/v4/encoding/MqttWriterV4.ts
@@ -0,0 +1,68 @@
+import { AppError } from "@src/AppError";
+import { DataWriter } from "../../shared/DataWriter";
+
+export class MqttWriterV4 extends DataWriter {
+  /**
+   * Creates a new instance of the MqttWriterV4 class with the specified capacity.
+   * @param capacity The total capacity of the buffer in bytes. Must be greater than zero.
+   */
+  public constructor(capacity: number) {
+    super(capacity);
+  }
+
+  /**
+   * Writes a One Byte Integer to the buffer.
+   * @param value The value to be written as a One Byte Integer. Must be between 0 and 255 (0xff).
+   * @throws If the value is outside the valid range for a One Byte Integer.
+   */
+  public writeOneByteInteger(value: number): void {
+    if (value < 0 || value > 0xff)
+      throw new AppError(
+        "Value must be between 0 and 255 for One Byte Integer"
+      );
+
+    this.write(value);
+  }
+
+  /**
+   * Writes a Two Byte Integer to the buffer.
+   * @param value The value to be written as a Two Byte Integer. Must be between 0 and 65535 (0xffff).
+   * @throws If the value is outside the valid range for a Two Byte Integer.
+   */
+  public writeTwoByteInteger(value: number): void {
+    if (value < 0 || value > 0xffff)
+      throw new AppError(
+        "Value must be between 0 and 65535 for Two Byte Integer"
+      );
+
+    const msb = value >> 8;
+    const lsb = value & 0x00ff;
+
+    this.write(msb);
+    this.write(lsb);
+  }
+
+  /**
+   * Writes a Variable Byte Integer to the buffer.
+   * @param value The value to be written as a Variable Byte Integer. Must be between 0 and 268435455 (0xffff_ff7f).
+   * @throws If the value is outside the valid range for a Variable Byte Integer.
+   */
+  public writeVariableByteInteger(value: number): void {
+    if (value < 0 || value > 0xff_ff_ff_7f)
+      throw new AppError(
+        "Value must be between 0 and 268435455 for Variable Byte Integer"
+      );
+
+    do {
+      let encodedByte = value % 0x80;
+      value = (value / 0x80) >> 0;
+
+      // if there are more data to encode, set the top bit of this byte
+      if (value > 0) {
+        encodedByte = encodedByte | 0x80;
+      }
+
+      this.write(encodedByte);
+    } while (value > 0);
+  }
+}
---
+++ tests/unit/mqtt/protocol/v4/encoding/MqttWriterV4.writeInteger.test.ts
@@ -0,0 +1,67 @@
+import { MqttWriterV4 } from "@src/mqtt/protocol/v4/encoding/MqttWriterV4";
+import { describe, expect, it } from "vitest";
+
+describe("MqttWriterV4", () => {
+  describe("writeOneByteInteger()", () => {
+    [
+      { input: 0x00, expected: [0x00] },
+      { input: 0x03, expected: [0x03] },
+      { input: 0x51, expected: [0x51] },
+      { input: 0x70, expected: [0x70] },
+      { input: 0xff, expected: [0xff] },
+    ].forEach(({ input, expected }) => {
+      it(`encodes ${input} to ${expected}`, () => {
+        const writer = new MqttWriterV4(expected.length);
+
+        writer.writeOneByteInteger(input);
+
+        expect(writer.toUint8Array()).toStrictEqual(Uint8Array.from(expected));
+      });
+    });
+  });
+
+  describe("writeTwoByteInteger()", () => {
+    [
+      { input: 0x0000, expected: [0x00, 0x00] },
+      { input: 0x000b, expected: [0x00, 0x0b] },
+      { input: 0x0100, expected: [0x01, 0x00] },
+      { input: 0x0d0e, expected: [0x0d, 0x0e] },
+      { input: 0xffff, expected: [0xff, 0xff] },
+    ].forEach(({ input, expected }) => {
+      it(`encodes ${input} to ${expected}`, () => {
+        const writer = new MqttWriterV4(expected.length);
+
+        writer.writeTwoByteInteger(input);
+
+        expect(writer.toUint8Array()).toStrictEqual(Uint8Array.from(expected));
+      });
+    });
+  });
+
+  describe("writeVariableByteInteger()", () => {
+    [
+      { input: 0x00, expected: [0x00] },
+      { input: 0x50, expected: [0x50] },
+      { input: 0x7f, expected: [0x7f] },
+
+      { input: 0x0080, expected: [0x80, 0x01] },
+      { input: 0x00ff, expected: [0xff, 0x01] },
+      { input: 0x0100, expected: [0x80, 0x02] },
+      { input: 0x3fff, expected: [0xff, 0x7f] },
+
+      { input: 0x004000, expected: [0x80, 0x80, 0x01] },
+      { input: 0x1fffff, expected: [0xff, 0xff, 0x7f] },
+
+      { input: 0x00200000, expected: [0x80, 0x80, 0x80, 0x01] },
+      { input: 0x0fffffff, expected: [0xff, 0xff, 0xff, 0x7f] },
+    ].forEach(({ input, expected }) => {
+      it(`encodes ${input} to ${expected}`, () => {
+        const writer = new MqttWriterV4(expected.length);
+
+        writer.writeVariableByteInteger(input);
+
+        expect(writer.toUint8Array()).toStrictEqual(Uint8Array.from(expected));
+      });
+    });
+  });
+});
