9140ba9d8e3775aceac03ddee7ef2f38cc890ae9
Grzegorz Łagocki <grzesieklagocki@gmail.com>
2026-07-01 21:26:23 +0200

    feat(encodeVariableHeaderV4): Implement `encodeVariableHeaderV4` function with unit tests

---
+++ src/mqtt/protocol/v4/encoding/encodeVariableHeaderV4.ts
@@ -0,0 +1,131 @@
+import { PacketType } from "../../shared/types";
+import {
+  AnyPacketV4,
+  ConnackPacketV4,
+  ConnectFlagsV4,
+  ConnectPacketV4,
+  PublishPacketV4,
+} from "../types";
+import { encodeStringUtf8 } from "./encodeStringUtf8";
+import { MqttWriterV4 } from "./MqttWriterV4";
+
+/**
+ * Encodes the variable header of an MQTT 3.1.1 packet into a Uint8Array.
+ * @param packet - The MQTT packet whose variable header is to be encoded.
+ * @returns A Uint8Array representing the encoded variable header of the MQTT packet.
+ */
+export function encodeVariableHeaderV4(packet: AnyPacketV4): Uint8Array {
+  switch (packet.typeId) {
+    case PacketType.PINGREQ:
+    case PacketType.PINGRESP:
+    case PacketType.DISCONNECT:
+      return encodeEmptyVariableHeader();
+    case PacketType.CONNACK:
+      return encodeConnackVariableHeader(packet);
+    case PacketType.PUBLISH:
+      return encodePublishVariableHeader(packet);
+    case PacketType.CONNECT:
+      return encodeConnectVariableHeader(packet);
+    default:
+      return encodeIdentifier(packet.identifier);
+  }
+}
+
+//
+// encoding functions
+//
+
+/**
+ * Encodes an empty variable header for MQTT packets that do not require a variable header.
+ * @returns A Uint8Array representing an empty variable header.
+ */
+const encodeEmptyVariableHeader = () => new Uint8Array();
+
+/**
+ * Encodes the variable header for a CONNACK packet.
+ * @param packet - The CONNACK packet to encode.
+ * @returns A Uint8Array representing the encoded variable header of the CONNACK packet.
+ */
+const encodeConnackVariableHeader = (packet: ConnackPacketV4): Uint8Array =>
+  new Uint8Array([
+    packet.sessionPresentFlag ? 1 : 0, // session present flag
+    packet.connectReturnCode, // connect return code
+  ]);
+
+/**
+ * Encodes the variable header for a PUBLISH packet.
+ * @param packet - The PUBLISH packet to encode.
+ * @returns A Uint8Array representing the encoded variable header of the PUBLISH packet.
+ */
+const encodePublishVariableHeader = (packet: PublishPacketV4): Uint8Array => {
+  const encodedTopic = encodeStringUtf8(packet.topicName);
+
+  const identifierLength = packet.identifier ? 2 : 0;
+  const topicLength = 2 + encodedTopic.length; // 2 bytes for the topic length prefix + topic name length
+  const variableHeaderLength = topicLength + identifierLength; // total length of the variable header (topic + optional identifier)
+
+  const writer = new MqttWriterV4(variableHeaderLength);
+
+  // write topic name
+  writer.writeBinaryData(encodedTopic);
+
+  // write identifier (if present)
+  if (packet.identifier !== undefined)
+    writer.writeTwoByteInteger(packet.identifier);
+
+  return writer.toUint8Array();
+};
+
+/**
+ * Encodes the variable header for a CONNECT packet.
+ * @param packet - The CONNECT packet to encode.
+ * @returns A Uint8Array representing the encoded variable header of the CONNECT packet.
+ */
+const encodeConnectVariableHeader = (packet: ConnectPacketV4): Uint8Array => {
+  const writer = new MqttWriterV4(10); // 10 bytes for the variable header of CONNECT packet
+
+  writer.writeString(packet.protocol.name);
+  writer.writeOneByteInteger(packet.protocol.level);
+  writer.writeOneByteInteger(connectFlagsToNumber(packet.flags));
+  writer.writeTwoByteInteger(packet.keepAlive);
+
+  return writer.toUint8Array();
+};
+
+/**
+ * Encodes a two-byte identifier into a Uint8Array.
+ * @param identifier - The identifier to encode.
+ * @returns A Uint8Array representing the encoded identifier.
+ */
+const encodeIdentifier = (identifier: number): Uint8Array =>
+  new Uint8Array([
+    (identifier & 0xff00) >> 8, // MSB
+    identifier & 0x00ff, // LSB
+  ]);
+
+//
+// helpers
+//
+
+/**
+ * Converts the ConnectFlagsV4 object into a number (for encoding).
+ * @param flags - The ConnectFlagsV4 object containing the flags to convert.
+ * @returns A number representing the encoded connect flags.
+ */
+const connectFlagsToNumber = (flags: ConnectFlagsV4): number => {
+  const userName = flags.userName ? 1 : 0;
+  const password = flags.password ? 1 : 0;
+  const willRetain = flags.willRetain ? 1 : 0;
+  const willQoS = flags.willQoS;
+  const willFlag = flags.willFlag ? 1 : 0;
+  const cleanSession = flags.cleanSession ? 1 : 0;
+
+  return (
+    (userName << 7) |
+    (password << 6) |
+    (willRetain << 5) |
+    (willQoS << 3) |
+    (willFlag << 2) |
+    (cleanSession << 1)
+  );
+};
---
+++ tests/unit/mqtt/protocol/v4/encoding/encodeVariableHeaderV4.test.ts
@@ -0,0 +1,457 @@
+import { describe, expect, it } from "vitest";
+import { PacketType } from "@src/mqtt/protocol/shared/types";
+import {
+  MqttPacketV4Factory,
+  PacketWithIdentifierV4Type,
+  SimplePacketV4Type,
+} from "@src/mqtt/protocol/v4/MqttPacketV4Factory";
+import { encodeVariableHeaderV4 } from "@src/mqtt/protocol/v4/encoding/encodeVariableHeaderV4";
+import {
+  ConnackReturnCodeV4,
+  ConnectFlagsV4,
+  ConnectionPayloadV4,
+  PublishFlagsV4,
+  SubackReturnCodeV4,
+  SubscriptionV4,
+} from "@src/mqtt/protocol/v4/types";
+
+const expectBytes = (actual: Uint8Array, expected: number[]) => {
+  expect([...actual]).toEqual(expected);
+};
+
+describe("encodeVariableHeaderV4", () => {
+  describe("simple packets", () => {
+    [PacketType.PINGREQ, PacketType.PINGRESP, PacketType.DISCONNECT].forEach(
+      (packetType) => {
+        it(`should encode empty variable header for ${PacketType[packetType]}`, () => {
+          const packet = MqttPacketV4Factory.createSimplePacketV4(
+            packetType as SimplePacketV4Type
+          );
+
+          const result = encodeVariableHeaderV4(packet);
+
+          expectBytes(result, []);
+        });
+      }
+    );
+  });
+
+  describe("packets with identifier", () => {
+    [
+      PacketType.PUBACK,
+      PacketType.PUBREC,
+      PacketType.PUBREL,
+      PacketType.PUBCOMP,
+      PacketType.UNSUBACK,
+    ].forEach((packetType) => {
+      it(`should encode packet identifier for ${PacketType[packetType]}`, () => {
+        const packet = MqttPacketV4Factory.createPacketWithIdentifierV4(
+          packetType as PacketWithIdentifierV4Type,
+          0x7641
+        );
+
+        const result = encodeVariableHeaderV4(packet);
+
+        expectBytes(result, [0x76, 0x41]);
+      });
+    });
+
+    it("should encode packet identifier 0x0000", () => {
+      const packet = MqttPacketV4Factory.createPacketWithIdentifierV4(
+        PacketType.PUBACK,
+        0
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(result, [0x00, 0x00]);
+    });
+
+    it("should encode packet identifier 0xa3c9", () => {
+      const packet = MqttPacketV4Factory.createPacketWithIdentifierV4(
+        PacketType.PUBACK,
+        0xa3c9
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(result, [0xa3, 0xc9]);
+    });
+
+    it("should encode maximum packet identifier 0xffff", () => {
+      const packet = MqttPacketV4Factory.createPacketWithIdentifierV4(
+        PacketType.PUBACK,
+        0xffff
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(result, [0xff, 0xff]);
+    });
+  });
+
+  describe("CONNECT", () => {
+    it("should encode minimal CONNECT variable header", () => {
+      const flags: ConnectFlagsV4 = {
+        userName: false,
+        password: false,
+        willRetain: false,
+        willQoS: 0,
+        willFlag: false,
+        cleanSession: true,
+      };
+
+      const payload: ConnectionPayloadV4 = {
+        clientIdentifier: "id",
+      };
+
+      const packet = MqttPacketV4Factory.createConnectPacketV4(
+        flags,
+        60,
+        payload
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(
+        result,
+        [
+          // Protocol Name: "MQTT"
+          0x00, 0x04, 0x4d, 0x51, 0x54, 0x54,
+
+          // Protocol Level
+          0x04,
+
+          // Connect Flags: Clean Session
+          0b00000010,
+
+          // Keep Alive = 60
+          0x00, 0x3c,
+        ]
+      );
+    });
+
+    it("should encode CONNECT variable header with will, username and password flags", () => {
+      const flags: ConnectFlagsV4 = {
+        userName: true,
+        password: true,
+        willRetain: false,
+        willQoS: 1,
+        willFlag: true,
+        cleanSession: true,
+      };
+
+      const payload: ConnectionPayloadV4 = {
+        clientIdentifier: "id",
+        willTopic: "/",
+        willMessage: new Uint8Array([0xfc]),
+        userName: "user",
+        password: new Uint8Array([0xbb]),
+      };
+
+      const packet = MqttPacketV4Factory.createConnectPacketV4(
+        flags,
+        220,
+        payload
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(
+        result,
+        [
+          // Protocol Name: "MQTT"
+          0x00, 0x04, 0x4d, 0x51, 0x54, 0x54,
+
+          // Protocol Level
+          0x04,
+
+          // Connect Flags:
+          // username=1, password=1, willRetain=0, willQoS=01,
+          // willFlag=1, cleanSession=1, reserved=0
+          0b11001110,
+
+          // Keep Alive = 220
+          0x00, 0xdc,
+        ]
+      );
+    });
+
+    it("should encode CONNECT variable header with clean session false", () => {
+      const flags: ConnectFlagsV4 = {
+        userName: false,
+        password: false,
+        willRetain: false,
+        willQoS: 0,
+        willFlag: false,
+        cleanSession: false,
+      };
+
+      const payload: ConnectionPayloadV4 = {
+        clientIdentifier: "persistent-client",
+      };
+
+      const packet = MqttPacketV4Factory.createConnectPacketV4(
+        flags,
+        30,
+        payload
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(
+        result,
+        [0x00, 0x04, 0x4d, 0x51, 0x54, 0x54, 0x04, 0b00000000, 0x00, 0x1e]
+      );
+    });
+
+    it("should encode CONNECT variable header with keep alive 0", () => {
+      const flags: ConnectFlagsV4 = {
+        userName: false,
+        password: false,
+        willRetain: false,
+        willQoS: 0,
+        willFlag: false,
+        cleanSession: true,
+      };
+
+      const payload: ConnectionPayloadV4 = {
+        clientIdentifier: "id",
+      };
+
+      const packet = MqttPacketV4Factory.createConnectPacketV4(
+        flags,
+        0,
+        payload
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(
+        result,
+        [0x00, 0x04, 0x4d, 0x51, 0x54, 0x54, 0x04, 0b00000010, 0x00, 0x00]
+      );
+    });
+
+    it("should encode CONNECT variable header with maximum keep alive", () => {
+      const flags: ConnectFlagsV4 = {
+        userName: false,
+        password: false,
+        willRetain: false,
+        willQoS: 0,
+        willFlag: false,
+        cleanSession: true,
+      };
+
+      const payload: ConnectionPayloadV4 = {
+        clientIdentifier: "id",
+      };
+
+      const packet = MqttPacketV4Factory.createConnectPacketV4(
+        flags,
+        0xffff,
+        payload
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(
+        result,
+        [0x00, 0x04, 0x4d, 0x51, 0x54, 0x54, 0x04, 0b00000010, 0xff, 0xff]
+      );
+    });
+  });
+
+  describe("CONNACK", () => {
+    it("should encode CONNACK variable header with session present false", () => {
+      const packet = MqttPacketV4Factory.createConnackPacketV4(
+        false,
+        ConnackReturnCodeV4.CONNECTION_ACCEPTED
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(result, [
+        0x00, // Acknowledge Flags
+        0x00, // Return Code
+      ]);
+    });
+
+    it("should encode CONNACK variable header with session present true", () => {
+      const packet = MqttPacketV4Factory.createConnackPacketV4(
+        true,
+        ConnackReturnCodeV4.CONNECTION_ACCEPTED
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(result, [
+        0x01, // Session Present
+        0x00,
+      ]);
+    });
+
+    it("should encode CONNACK variable header with refused return code", () => {
+      const packet = MqttPacketV4Factory.createConnackPacketV4(
+        false,
+        ConnackReturnCodeV4.CONNECTION_REFUSED_BAD_USER_NAME_OR_PASSWORD
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(result, [
+        0x00,
+        ConnackReturnCodeV4.CONNECTION_REFUSED_BAD_USER_NAME_OR_PASSWORD,
+      ]);
+    });
+  });
+
+  describe("PUBLISH", () => {
+    it("should encode PUBLISH variable header for QoS 0 without identifier", () => {
+      const flags: PublishFlagsV4 = {
+        dup: false,
+        qosLevel: 0,
+        retain: false,
+      };
+
+      const packet = MqttPacketV4Factory.createPublishPacketV4(
+        new Uint8Array([0x10, 0x20]),
+        flags,
+        "a"
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(
+        result,
+        [
+          // Topic Name: "a"
+          0x00, 0x01, 0x61,
+        ]
+      );
+    });
+
+    it("should encode PUBLISH variable header for QoS 1 with identifier", () => {
+      const flags: PublishFlagsV4 = {
+        dup: false,
+        qosLevel: 1,
+        retain: false,
+      };
+
+      const packet = MqttPacketV4Factory.createPublishPacketV4(
+        new Uint8Array([0x10, 0x20]),
+        flags,
+        "a",
+        0x1234
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(
+        result,
+        [
+          // Topic Name: "a"
+          0x00, 0x01, 0x61,
+
+          // Packet Identifier
+          0x12, 0x34,
+        ]
+      );
+    });
+
+    it("should encode PUBLISH variable header for QoS 2 with identifier", () => {
+      const flags: PublishFlagsV4 = {
+        dup: false,
+        qosLevel: 2,
+        retain: false,
+      };
+
+      const packet = MqttPacketV4Factory.createPublishPacketV4(
+        new Uint8Array([0xaa]),
+        flags,
+        "t/a",
+        0x0001
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(
+        result,
+        [
+          // Topic Name: "t/a"
+          0x00, 0x03, 0x74, 0x2f, 0x61,
+
+          // Packet Identifier
+          0x00, 0x01,
+        ]
+      );
+    });
+
+    it("should encode UTF-8 topic name", () => {
+      const flags: PublishFlagsV4 = {
+        dup: false,
+        qosLevel: 0,
+        retain: false,
+      };
+
+      const packet = MqttPacketV4Factory.createPublishPacketV4(
+        new Uint8Array([]),
+        flags,
+        "ąć"
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(
+        result,
+        [
+          // "ąć" UTF-8: C4 85 C4 87, length = 4 bytes
+          0x00, 0x04, 0xc4, 0x85, 0xc4, 0x87,
+        ]
+      );
+    });
+  });
+
+  describe("SUBSCRIBE", () => {
+    it("should encode SUBSCRIBE variable header", () => {
+      const subscriptions: SubscriptionV4[] = [["a/b", 1]];
+
+      const packet = MqttPacketV4Factory.createSubscribePacketV4(
+        0x1234,
+        subscriptions
+      );
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(result, [0x12, 0x34]);
+    });
+  });
+
+  describe("SUBACK", () => {
+    it("should encode SUBACK variable header", () => {
+      const packet = MqttPacketV4Factory.createSubackPacketV4(0x1234, [
+        SubackReturnCodeV4.SUCCESS_MAXIMUM_QOS_0,
+        SubackReturnCodeV4.SUCCESS_MAXIMUM_QOS_1,
+        SubackReturnCodeV4.SUCCESS_MAXIMUM_QOS_2,
+        SubackReturnCodeV4.FAILURE,
+      ]);
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(result, [0x12, 0x34]);
+    });
+  });
+
+  describe("UNSUBSCRIBE", () => {
+    it("should encode UNSUBSCRIBE variable header", () => {
+      const packet = MqttPacketV4Factory.createUnsubscribePacketV4(0x1234, [
+        "a/b",
+        "c/#",
+      ]);
+
+      const result = encodeVariableHeaderV4(packet);
+
+      expectBytes(result, [0x12, 0x34]);
+    });
+  });
+});
