84aecc403d8095b6976a7e25303db0233e0163a8
Grzegorz Łagocki <grzesieklagocki@gmail.com>
2026-07-08 01:51:25 +0200

    refactor(validation): Refactor validation logic in `_assertValidSubscribePayloadV4`

--- src/mqtt/protocol/v4/validation/subscribe.ts
+++ src/mqtt/protocol/v4/validation/subscribe.ts
@@ -1,5 +1,5 @@
 import { AppError } from "@src/AppError";
-import { SubscribePacketV4 } from "../types";
+import { SubscribePacketV4, SubscriptionV4 } from "../types";
 import { _assertValidTopicFilter, _assertValidTopicName } from "./topic";
 
 /**
@@ -8,40 +8,51 @@ import { _assertValidTopicFilter, _assertValidTopicName } from "./topic";
  * @throws AppError if the packet is invalid.
  */
 export function _assertValidSubscribePayloadV4(packet: SubscribePacketV4) {
-  // The Topic Filters in a SUBSCRIBE packet payload MUST be UTF-8 encoded strings as defined in Section 1.5.3.
-  // [MQTT-3.8.3-1]
+  _assertValidSubscriptionListLength(packet.subscriptionList.length);
+
   packet.subscriptionList.forEach((subscription) => {
-    const topicFilter = subscription[0];
-
-    if (typeof topicFilter !== "string")
-      throw new AppError(
-        `Invalid topic filter in SUBSCRIBE packet: ${topicFilter}. Topic Filters must be UTF-8 encoded strings [MQTT-3.8.3-1]`
-      );
+    _assertValidSubscription(subscription);
   });
+}
 
+/**
+ * Asserts that the given subscription list length is valid according to MQTT v4 specs.
+ * @param length - The length of the subscription list to validate.
+ */
+export function _assertValidSubscriptionListLength(length: number) {
   // The payload of a SUBSCRIBE packet MUST contain at least one Topic Filter / QoS pair. A SUBSCRIBE packet with no payload is a protocol violation.
   // [MQTT-3.8.3-3]
-  if (packet.subscriptionList.length === 0)
+  if (length === 0)
     throw new AppError(
       "The payload of a SUBSCRIBE packet MUST contain at least one Topic Filter / QoS pair. A SUBSCRIBE packet with no payload is a protocol violation [MQTT-3.8.3-3]"
+    );
+}
+
+/**
+ * Asserts that the given subscription is valid according to MQTT v4 specs.
+ * @param subscription - The subscription to validate.
+ */
+export function _assertValidSubscription(
+  subscription: [topicFilter: string, qos: number]
+): asserts subscription is SubscriptionV4 {
+  const topic = subscription[0];
+  const qos = subscription[1];
+
+  // The Topic Filters in a SUBSCRIBE packet payload MUST be UTF-8 encoded strings as defined in Section 1.5.3.
+  // [MQTT-3.8.3-1]
+  if (typeof topic !== "string")
+    throw new AppError(
+      `Invalid topic filter in SUBSCRIBE packet: ${topic}. Topic Filters must be UTF-8 encoded strings [MQTT-3.8.3-1]`
     );
 
   // The Server MUST treat a SUBSCRIBE packet as malformed and close the Network Connection if any of Reserved bits in the payload are non-zero, or QoS is not 0,1 or 2.
   // [MQTT-3-8.3-4]
-  packet.subscriptionList.forEach((subscription) => {
-    const qos = subscription[1];
-
-    if (qos !== 0 && qos !== 1 && qos !== 2)
-      throw new AppError(
-        `Invalid QoS level in SUBSCRIBE packet: ${qos}. Valid QoS levels are 0, 1, or 2 [MQTT-3.8.3-4]`
-      );
-  });
+  if (qos !== 0 && qos !== 1 && qos !== 2)
+    throw new AppError(
+      `Invalid QoS level in SUBSCRIBE packet: ${qos}. Valid QoS levels are 0, 1, or 2 [MQTT-3.8.3-4]`
+    );
 
   // All Topic Names and Topic Filters MUST be at least one character long.
   // [MQTT-4.7.3-1]
-  packet.subscriptionList.forEach((subscription) => {
-    const topic = subscription[0];
-
-    _assertValidTopicFilter(topic);
-  });
+  _assertValidTopicFilter(topic);
 }
