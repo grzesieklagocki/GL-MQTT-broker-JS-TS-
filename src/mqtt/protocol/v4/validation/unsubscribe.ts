f15185ab600cb8e8735a325b04c68889319870b6
Grzegorz Łagocki <grzesieklagocki@gmail.com>
2026-07-08 02:11:54 +0200

    refactor(validation): Refactor validation logic for UNSUBSCRIBE packets

--- src/mqtt/protocol/v4/validation/unsubscribe.ts
+++ src/mqtt/protocol/v4/validation/unsubscribe.ts
@@ -1,6 +1,6 @@
 import { AppError } from "@src/AppError";
 import { UnsubscribePacketV4 } from "../types";
-import { _assertValidTopicFilter, _assertValidTopicName } from "./topic";
+import { _assertValidTopicFilter } from "./topic";
 
 /**
  * Asserts that an UNSUBSCRIBE packet is valid according to MQTT 3.1.1 specification.
@@ -8,26 +8,43 @@ import { _assertValidTopicFilter, _assertValidTopicName } from "./topic";
  * @throws AppError if the packet is invalid.
  */
 export function _assertValidUnsubscribePayloadV4(packet: UnsubscribePacketV4) {
+  _assertValidUnsubscribeTopicFilterListLength(packet.topicFilterList.length);
   // The Topic Filters in an UNSUBSCRIBE packet MUST be UTF-8 encoded strings as defined in Section 1.5.3, packed contiguously.
   // [MQTT-3.10.3-1]
   packet.topicFilterList.forEach((topicFilter) => {
-    if (typeof topicFilter !== "string")
-      throw new AppError(
-        `Invalid topic filter in UNSUBSCRIBE packet: ${topicFilter}. Topic Filters must be UTF-8 encoded strings [MQTT-3.10.3-1]`
-      );
+    _assertValidUnsubscribeTopicFilter(topicFilter);
   });
+}
 
+/**
+ * Asserts that the given topic filter list length is valid according to MQTT v4 specs.
+ * @param length - The length of the topic filter list to validate.
+ * @throws AppError if the topic filter list length is invalid.
+ */
+export function _assertValidUnsubscribeTopicFilterListLength(length: number) {
   // The Payload of an UNSUBSCRIBE packet MUST contain at least one Topic Filter.
   // An UNSUBSCRIBE packet with no payload is a protocol violation.
   // [MQTT-3.10.3-2]
-  if (packet.topicFilterList.length === 0)
+  if (length === 0)
     throw new AppError(
       "The Payload of an UNSUBSCRIBE packet MUST contain at least one Topic Filter. An UNSUBSCRIBE packet with no payload is a protocol violation [MQTT-3.10.3-2]"
+    );
+}
+
+/**
+ * Asserts that the given topic filter is valid according to MQTT v4 specs.
+ * @param topicFilter - The topic filter to validate.
+ * @throws AppError if the topic filter is invalid.
+ */
+export function _assertValidUnsubscribeTopicFilter(topicFilter: string) {
+  // The Topic Filters in an UNSUBSCRIBE packet MUST be UTF-8 encoded strings as defined in Section 1.5.3, packed contiguously.
+  // [MQTT-3.10.3-1]
+  if (typeof topicFilter !== "string")
+    throw new AppError(
+      `Invalid topic filter in UNSUBSCRIBE packet: ${topicFilter}. Topic Filters must be UTF-8 encoded strings [MQTT-3.10.3-1]`
     );
 
   // All Topic Names and Topic Filters MUST be at least one character long.
   // [MQTT-4.7.3-1]
-  packet.topicFilterList.forEach((topic) => {
-    _assertValidTopicFilter(topic);
-  });
+  _assertValidTopicFilter(topicFilter);
 }
