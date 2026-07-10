969ec06194004d8da873dd06e1b77e8b2c9e8354
Grzegorz Łagocki <grzesieklagocki@gmail.com>
2026-07-10 04:46:33 +0200

    feat(client): Implement ClientV4 class stub for MQTT client logic management

---
+++ src/mqtt/client/v4/client.ts
@@ -0,0 +1,29 @@
+import { AppError } from "@src/AppError";
+import { ITransportAdapterV4 } from "./types";
+import { IPacketIdentifierManager } from "@mqtt/shared/types";
+import { ConnectionStatus } from "../shared/types";
+
+export class ClientV4 {
+  private mqttConnectionStatus: ConnectionStatus =
+    ConnectionStatus.DISCONNECTED;
+
+  /**
+   * Creates an instance of ClientV4.
+   * @param transport - The transport adapter responsible for sending and receiving MQTT packets.
+   * @param packetIdManager - The packet identifier manager responsible for generating unique packet identifiers.
+   */
+  public constructor(
+    private readonly transport: ITransportAdapterV4,
+    private readonly packetIdManager: IPacketIdentifierManager
+  ) {}
+
+  /**
+   * Asserts that the MQTT client is currently connected.
+   */
+  private _assertClientConnected() {
+    if (this.mqttConnectionStatus !== ConnectionStatus.CONNECTED)
+      throw new AppError(
+        `Client is not connected. Current status: ${ConnectionStatus[this.mqttConnectionStatus]}`
+      );
+  }
+}
